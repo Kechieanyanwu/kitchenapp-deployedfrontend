a448cee60b6a0f03b60cc95b24a91614d4cb5a9b
Nkechi Anyanwu <kechie.94@gmail.com>
2024-06-26 10:27:11 +0100

    refactoring to use env variables

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -23,7 +23,7 @@ export default function LoginForm() {
         setError(null); //clear previous errors
         
         try {
-            const response = await fetch('https://kitchen-app-backend-two.vercel.app/login', {
+            const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/login`, {
                 method: 'POST',
                 headers: {
                     'content-type': 'application/json',
