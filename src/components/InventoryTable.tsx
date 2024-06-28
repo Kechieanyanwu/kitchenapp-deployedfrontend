9522f57b174a17b67826c15ea564cfda727b552c
Nkechi Anyanwu <kechie.94@gmail.com>
2024-06-28 14:10:23 +0100

    Included effect to fetch inventory items after page load

--- src/components/InventoryTable.tsx
+++ src/components/InventoryTable.tsx
@@ -1,17 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { InventoryObject } from "../utils/interfaces";
 
 export default function InventoryTable() {
   //will run function to get checklist items, then will set the state
-  const inventoryItems: Array<InventoryObject> = [
-    { id: 1, item_name: "Dishwashing tabs", quantity: 10, category_id: 3, user_id: 1 },
-    { id: 2, item_name: "Water bottle", quantity: 13, category_id: 3, user_id: 1 },
-    { id: 3, item_name: "Ipad", quantity: 1, category_id: 3, user_id: 1 },
-    { id: 4, item_name: "Deloitte", quantity: 3, category_id: 3, user_id: 1 },
-    { id: 5, item_name: "Detergent", quantity: 30, category_id: 3, user_id: 1 }
-  ];
+  // const inventoryItems: Array<InventoryObject> = [
+  //   { id: 1, item_name: "Dishwashing tabs", quantity: 10, category_id: 3, user_id: 1 },
+  //   { id: 2, item_name: "Water bottle", quantity: 13, category_id: 3, user_id: 1 },
+  //   { id: 3, item_name: "Ipad", quantity: 1, category_id: 3, user_id: 1 },
+  //   { id: 4, item_name: "Deloitte", quantity: 3, category_id: 3, user_id: 1 },
+  //   { id: 5, item_name: "Detergent", quantity: 30, category_id: 3, user_id: 1 }
+  // ];
 
-  const [items, setItems] = useState(inventoryItems);
+  // const [items, setItems] = useState(inventoryItems);
+  const [items, setItems] = useState<Array<InventoryObject>>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => { //code duplication. Could refactor into a helper function
+    const fetchInventoryItems = async () => {
+      try {
+        const authToken = localStorage.getItem('auth_token');
+        const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/inventory`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `${authToken}`
+          },
+          credentials: 'include' // Set to 'same-origin' or 'include' as needed
+        });
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setItems(data);
+      } catch (error:any) { //is this right?
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchInventoryItems();
+  }, []);
+
 
   const checkItem = () => {
     //checks off an item in the backend
