398b00035636b77ed352e982fe458a12347f7a13
Nkechi Anyanwu <kechie.94@gmail.com>
2024-07-09 19:58:33 +0100

    basic framework of add item page

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,15 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Welcome from './pages/Welcome';
 import SignUp from './pages/SignUp';
+import AddItem from './pages/AddItem';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-         <Route path="/checklist" element={<Checklist />} />
+        <Route path="/checklist" element={<Checklist />} />
+        <Route path="/checklist/additem" element={<AddItem />} />
         <Route path="/inventory" element={<Inventory />} />
         <Route path="/home" element={<Home />} />
         <Route path="/" element={<Welcome />} />
---
+++ src/pages/AddItem.tsx
@@ -0,0 +1,35 @@
+import PageHeader from "../components/PageHeader";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+
+export default function AddItem() {
+    return <div>
+        <PageHeader title={"Checklist"} subtitle={"Create a Checklist so you don't miss a thing!"} />
+            <div className="curved-section">
+                <h1>Add Item</h1>
+                <Form.Group>
+                    <Form.Label>Item Name</Form.Label><br></br>
+                    <Form.Control placeholder="Enter item name" />
+                </Form.Group>
+                <InputGroup className="mb-3">
+                    <Button variant="outline-secondary" id="button-addon1">
+                    Button
+                    </Button>
+                    <Form.Control
+                    aria-label="Example text with button addon"
+                    aria-describedby="basic-addon1"
+                    />
+                    <Button variant="outline-secondary" id="button-addon1">
+                        Button
+                    </Button>
+                </InputGroup>
+                <Form.Select>
+                    <option>Select from dropdown</option>
+                    <option value="1">Fruit</option>
+                    <option value="2">Dairy</option>
+                    <option value="3">Bakery</option>
+                </Form.Select>
+            </div>
+    </div>
+}
